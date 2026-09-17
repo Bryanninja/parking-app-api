d969f7c519169f697e2b5378104c478e29ac2af7
bryan <bryannascimentopl800@gmail.com>
2026-09-17 11:30:49 -0300

    feat: add CreateCustomerController

---
+++ src/controllers/create-customer.js
@@ -0,0 +1,31 @@
+import z from 'zod';
+import { CustomerAlredyExistError } from '../errors/customer.js';
+import { badRequest, created, serverError } from '../helpers/http.js';
+import { createCustomerSchema } from '../schemas/customer-schema.js';
+
+export class CreateCustomerController {
+  constructor(createCustomerUseCase) {
+    this.createCustomerUseCase = createCustomerUseCase;
+  }
+
+  async execute(httpRequest) {
+    try {
+      // validar recebimento dos dados
+
+      const validateBody = createCustomerSchema.parse(httpRequest.body);
+
+      const customer = await this.createCustomerUseCase.execute(validateBody);
+
+      return created(customer);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return badRequest({ message: error.issues[0].message });
+      }
+      if (error instanceof CustomerAlredyExistError) {
+        return badRequest({ message: error.message });
+      }
+      console.error(error);
+      return serverError();
+    }
+  }
+}

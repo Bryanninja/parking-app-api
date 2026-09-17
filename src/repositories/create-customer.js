0b728f481bd44b19275edf531a8e2504a7b2a773
bryan <bryannascimentopl800@gmail.com>
2026-09-17 10:17:27 -0300

    feat: add CreateCustomerRepository

---
+++ src/repositories/create-customer.js
@@ -0,0 +1,10 @@
+import { prisma } from '../lib/prisma.js';
+
+export class PostgresCreateCustomerRepository {
+  async execute(customerParams) {
+    const customer = await prisma.customer.create({
+      data: customerParams,
+    });
+    return customer;
+  }
+}

f382f26a41df2e944f537fa7e362a33bb6840cfe
bryan <bryannascimentopl800@gmail.com>
2026-09-17 11:30:11 -0300

    feat: add helpers http

---
+++ src/helpers/http.js
@@ -0,0 +1,34 @@
+export const badRequest = (body) => {
+  return {
+    statusCode: 400,
+    body,
+  };
+};
+
+export const ok = (body) => {
+  return {
+    statusCode: 200,
+    body,
+  };
+};
+
+export const created = (body) => {
+  return {
+    statusCode: 201,
+    body,
+  };
+};
+
+export const notFound = (body) => {
+  return {
+    statusCode: 404,
+    body,
+  };
+};
+
+export const serverError = (body) => {
+  return {
+    statusCode: 500,
+    body,
+  };
+};

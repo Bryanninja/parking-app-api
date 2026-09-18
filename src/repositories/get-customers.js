571cf8dff29fd610ebdf4f9e15fdbf957b2b7d37
bryan <bryannascimentopl800@gmail.com>
2026-09-18 15:47:29 -0300

    feat: add PostgresGetCustomersRepository

---
+++ src/repositories/get-customers.js
@@ -0,0 +1,12 @@
+import { prisma } from '../lib/prisma.js';
+
+export class PostgresGetCustomersRepository {
+  async execute() {
+    const customers = await prisma.customer.findMany({
+      orderBy: {
+        created_at: 'desc',
+      },
+    });
+    return customers;
+  }
+}
--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,2 +1,3 @@
 export * from './create-customer.js';
 export * from './get-customer-by-phone.js';
+export * from './get-customers.js';

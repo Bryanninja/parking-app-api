e5d370566057092a4a24da60d0d299cb8f959cb1
bryan <bryannascimentopl800@gmail.com>
2026-09-19 16:27:03 -0300

    feat: ensure when update a customer At least a field must be informed

--- src/schemas/customer-schema.js
+++ src/schemas/customer-schema.js
@@ -20,18 +20,22 @@ export const deleteCustomerParamsSchema = z.object({
   customerId: z.uuid({ error: 'The Customer id has to be a valid UUID.' }),
 });
 
-export const updateCustomerSchema = z.object({
-  name: z
-    .string()
-    .trim()
-    .min(1, { message: 'O nome não pode estar vazio.' })
-    .optional(),
-  phone: z
-    .string()
-    .trim()
-    .min(1, { message: 'O telefone não pode estar vazio.' })
-    .optional(),
-});
+export const updateCustomerSchema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .min(1, { message: 'Name not be null .' })
+      .optional(),
+    phone: z
+      .string()
+      .trim()
+      .min(1, { message: 'Phone not be null.' })
+      .optional(),
+  })
+  .refine((data) => Object.keys(data).length > 0, {
+    error: 'At least a field must be informed.',
+  });
 export const updateCustomerParamsSchema = z.object({
   customerId: z.uuid({ message: 'The Customer id has to be a valid UUID.' }),
 });

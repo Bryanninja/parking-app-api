9e3f4c7f13101839d322b616347567667e42b754
bryan <bryannascimentopl800@gmail.com>
2026-09-21 12:02:38 -0300

    feat: add CreateTicketUseCase

---
+++ src/use-cases/ticket/create-ticket.js
@@ -0,0 +1,32 @@
+import { CustomerNotFoundError } from '../../errors/customer.js';
+import { VehicleAlreadyParkedError } from '../../errors/ticket.js';
+
+export class CreateTicketUseCase {
+  constructor(
+    createTicketRepository,
+    getCustomerByIdRepository,
+    getParkedTicketByLicensePlateRepository,
+  ) {
+    this.createTicketRepository = createTicketRepository;
+    this.getCustomerByIdRepository = getCustomerByIdRepository;
+    this.getParkedTicketByLicensePlateRepository =
+      getParkedTicketByLicensePlateRepository;
+  }
+  async execute(ticketParams) {
+    const customerId = ticketParams.customerId;
+    const licensePlate = ticketParams.licensePlate;
+
+    const customerExist =
+      await this.getCustomerByIdRepository.execute(customerId);
+
+    if (!customerExist) throw new CustomerNotFoundError(customerId);
+
+    const vehicleAlreadyParked =
+      await this.getParkedTicketByLicensePlateRepository.execute(licensePlate);
+    if (!vehicleAlreadyParked)
+      throw new VehicleAlreadyParkedError(licensePlate);
+
+    const ticket = this.createTicketRepository.execute(ticketParams);
+    return ticket;
+  }
+}

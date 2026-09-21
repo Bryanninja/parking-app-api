b052987f016684846ee4fc11ac9aef10b3258e6a
bryan <bryannascimentopl800@gmail.com>
2026-09-21 14:08:28 -0300

    feat: add createTicketSchema with licensePlateRegex BR

---
+++ src/schemas/ticket.js
@@ -0,0 +1,25 @@
+import z from 'zod';
+
+// Regex universal de placas brasileiras (antiga e Mercosul)
+const licensePlateRegex = /^[A-Z]{3}[0-9][0-9A-Z][0-9]{2}$/;
+
+export const createTicketSchema = z.object({
+  customer_id: z.string().uuid({
+    message: 'The Customer id has to be a valid UUID.',
+  }),
+
+  vehicle_model: z
+    .string({ error: 'Vehicle model is required.' })
+    .trim()
+    .min(1, { message: 'Vehicle model is required.' }),
+
+  license_plate: z
+    .string({ error: 'License plate is required.' })
+    .trim()
+    // 1. Deixa tudo maiúsculo e remove traços e espaços
+    .transform((val) => val.toUpperCase().replace(/[^A-Z0-9]/g, ''))
+    // 2. Valida com o Regex universal
+    .refine((val) => licensePlateRegex.test(val), {
+      message: 'Invalid license plate. accept formats: ABC1234 ou ABC1D23.',
+    }),
+});

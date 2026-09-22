ae950c5e504d5df45db8fce47cae1ea16b0071b6
bryan <bryannascimentopl800@gmail.com>
2026-09-22 09:48:11 -0300

    feat: add route to get all tickets

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -1,11 +1,20 @@
 import { Router } from 'express';
-import { makeCreateTicketController } from '../factories/tickets.js';
+import {
+  makeCreateTicketController,
+  makeGetTicketsController,
+} from '../factories/tickets.js';
 
 const ticketsRouter = Router();
 
 ticketsRouter.post('/', async (req, res) => {
   const createTicketController = makeCreateTicketController();
   const { statusCode, body } = await createTicketController.execute(req);
+  res.status(statusCode).json(body);
+});
+
+ticketsRouter.get('/', async (req, res) => {
+  const getTicketsController = makeGetTicketsController();
+  const { statusCode, body } = await getTicketsController.execute();
   res.status(statusCode).json(body);
 });
 

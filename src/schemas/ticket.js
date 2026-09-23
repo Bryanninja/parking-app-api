f1939a19fa5427b961ec946517cd5103bcc2aea1
bryan <bryannascimentopl800@gmail.com>
2026-09-23 10:54:12 -0300

    feat: add checkOutTicketParamsSchema and checkOutTicketSchema

--- src/schemas/ticket.js
+++ src/schemas/ticket.js
@@ -4,14 +4,14 @@ import z from 'zod';
 const licensePlateRegex = /^[A-Z]{3}[0-9][0-9A-Z][0-9]{2}$/;
 
 export const createTicketSchema = z.object({
-  customer_id: z.string().uuid({
-    message: 'The Customer id has to be a valid UUID.',
+  customer_id: z.uuid({
+    error: 'The Customer id has to be a valid UUID.',
   }),
 
   vehicle_model: z
     .string({ error: 'Vehicle model is required.' })
     .trim()
-    .min(1, { message: 'Vehicle model is required.' }),
+    .min(1, { error: 'Vehicle model is required.' }),
 
   license_plate: z
     .string({ error: 'License plate is required.' })
@@ -20,6 +20,16 @@ export const createTicketSchema = z.object({
     .transform((val) => val.toUpperCase().replace(/[^A-Z0-9]/g, ''))
     // 2. Valida com o Regex universal
     .refine((val) => licensePlateRegex.test(val), {
-      message: 'Invalid license plate. accept formats: ABC1234 ou ABC1D23.',
+      error: 'Invalid license plate. accept formats: ABC1234 ou ABC1D23.',
     }),
+});
+
+export const checkOutTicketParamsSchema = z.object({
+  id: z.uuid({ error: 'The id has to be a valid UUID.' }),
+});
+
+export const checkOutTicketSchema = z.object({
+  payment_method: z.enum('CASH', 'CARD', 'PIX', {
+    error: 'The payment methods allowed is: CASH, CARD or PIX',
+  }),
 });

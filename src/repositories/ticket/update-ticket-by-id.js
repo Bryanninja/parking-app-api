2523c5ef0595b2e7cec2eba65e7a02194cabeb19
bryan <bryannascimentopl800@gmail.com>
2026-09-23 10:33:22 -0300

    fix: ensure PostgresUpdateTicketByIdRepository receive ticketId

--- src/repositories/ticket/update-ticket-by-id.js
+++ src/repositories/ticket/update-ticket-by-id.js
@@ -1,8 +1,11 @@
 import { prisma } from '../../lib/prisma.js';
 
 export class PostgresUpdateTicketByIdRepository {
-  async execute(ticketParams) {
+  async execute(ticketId, ticketParams) {
     const ticket = await prisma.update({
+      where: {
+        id: ticketId,
+      },
       data: ticketParams,
     });
     return ticket;
